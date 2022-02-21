3c0a8b240ec928d6d5d70626ce376e701e85370a
naryv <naryv@DESKTOP-NB62JOJ>
2022-02-21 18:56:22 +0200

    Add project files.

---
+++ Lab01/DateCalculator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    static class DateCalculator
+    {
+        private static readonly string[] westernZodiacs = {"The Sea-goat", "The Water Bearer", "The Fish", "The Ram", "The Bull", "The Twins", 
+            "The Crab", "The Lion", "The Virgin", "The Scales", "The Scorpion", "The Archer"};
+        private static readonly string[] chineseZodiacs = {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox",
+            "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat"};
+
+        public static int GetAge(DateTime date)
+        {
+            int result = DateTime.Now.Year - date.Year;
+            if(DateTime.Now.Month < date.Month)
+            {
+                result--;
+            }
+            else if(DateTime.Now.Month == date.Month && DateTime.Now.Day < date.Day)
+            {
+                result--;
+            }
+            return result;
+        }
+
+        public static bool IsDateCorrect(DateTime date)
+        {
+            int age = GetAge(date);
+            return age >= 0 && age <= 135;
+        }
+
+        public static bool IsBirthday(DateTime date)
+        {
+            return DateTime.Now.Month == date.Month && DateTime.Now.Day == date.Day;
+        }
+
+        public static string GetChineseZodiac(DateTime date)
+        {
+            return chineseZodiacs[CalculateChineseZodiac(date)];
+        }
+
+        public static string GetWesternZodiac(DateTime date)
+        {
+            return westernZodiacs[CalculateWesternZodiac(date)];
+        }
+
+        private static int CalculateChineseZodiac(DateTime date)
+        {
+            return date.Year % 12;
+        }
+
+        private static int CalculateWesternZodiac(DateTime date)
+        {
+            switch(date.Month)
+            {
+                case 1:
+                    return date.Day <= 20 ? 0 : 1;
+                case 2:
+                    return date.Day <= 19 ? 1 : 2;
+                case 3:
+                    return date.Day <= 20 ? 2 : 3;
+                case 4:
+                    return date.Day <= 20 ? 3 : 4;
+                case 5:
+                    return date.Day <= 20 ? 4 : 5;
+                case 6:
+                    return date.Day <= 21 ? 5 : 6;
+                case 7:
+                    return date.Day <= 22 ? 6 : 7;
+                case 8:
+                    return date.Day <= 22 ? 7 : 8;
+                case 9:
+                    return date.Day <= 22 ? 8 : 9;
+                case 10:
+                    return date.Day <= 22 ? 9 : 10;
+                case 11:
+                    return date.Day <= 22 ? 10 : 11;
+                case 12:
+                    return date.Day <= 21 ? 11 : 0;
+                default:
+                    return 1;
+            }
+        }
+
+    }
+}
---
+++ Lab01/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DatePicker_DateChanged(object Sender, SelectionChangedEventArgs e)
+        {
+            if (!DateCalculator.IsDateCorrect((DateTime)datePicker.SelectedDate))
+            {
+                MessageBox.Show("Date is incorrect");
+                return;
+            }
+            if (DateCalculator.IsBirthday((DateTime)datePicker.SelectedDate))
+            {
+                MessageBox.Show("Happy Birthday!");
+            }
+            textBlock1.Text = DateCalculator.GetAge((DateTime)datePicker.SelectedDate).ToString();
+            textBlock2.Text = DateCalculator.GetWesternZodiac((DateTime)datePicker.SelectedDate);
+            textBlock3.Text = DateCalculator.GetChineseZodiac((DateTime)datePicker.SelectedDate);
+        }
+
+    }
+}
